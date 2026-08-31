8efe1b03fe87e71281380c07f1ceab31a79424f4
aren1toross <arentorossian28@gmail.com>
2026-08-31 18:41:09 -0400

    distance script: addded save button functionality

--- tools/distance/script.js
+++ tools/distance/script.js
@@ -1,4 +1,4 @@
-import { getGeneralSetting } from "../../storage.js";
+import { getGeneralSetting, getSave, saveData } from "../../storage.js";
 
 let newLocationRadiusIgnore = document.querySelector("#new-location-ignore-radius");
 let newLocationRadiusIgnoreOut = document.querySelector("#new-location-ignore-radius-out");
@@ -10,7 +10,7 @@ let startLocationSpan = document.querySelector("#start-location");
 let currentLocationSpan = document.querySelector("#current-location");
 let distanceFromStartSpan = document.querySelector("#distance-from-start");
 let previousPointsListElem = document.querySelector("#previous-points");
-console.log(previousPointsListElem);
+let historyContainer = document.querySelector("#history-container");
 
 let distanceValues = {
     isTracking: false,
@@ -26,6 +26,15 @@ let geolocationOptions = {
     timeout: 5000,
     maximumAge: 0,
 };
+let dateLocalOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "2-digit"
+};
+
 
 let chosenUnit = getGeneralSetting("distance-unit");
 
@@ -52,6 +61,73 @@ function getChosenUnitDistanceString(distance) {
     }
 }
 
+function copyButton(e, points) {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(JSON.stringify(points))
+        .then(() => {
+            e.target.textContent = "Copied!";
+            setTimeout(function() {
+                e.target.textContent = "Copy points";
+            }, 3000);
+        });
+    }
+}
+
+function addMeasureToHistory(date, distance, points) {
+    let historyItem = document.createElement("div");
+    historyItem.classList.add("history-item");
+
+    let dateP = document.createElement("p");
+    dateP.classList.add("history-item-date");
+    dateP.innerHTML = `<b>Date:</b> ${new Date(date).toLocaleDateString("en-CA", dateLocalOptions)}`;
+    historyItem.appendChild(dateP);
+
+    let distanceP = document.createElement("p");
+    distanceP.classList.add("history-item-distance");
+    distanceP.innerHTML = `<b>Distance:</b> ${getChosenUnitDistanceString(distance)}`;
+    historyItem.appendChild(distanceP);
+
+    let pointsDetails = document.createElement("details");
+    let pointsSummary = document.createElement("summary");
+    pointsSummary.innerHTML = "<b>Saved points:</b>";
+    pointsDetails.appendChild(pointsSummary);
+
+    let copyBtn = document.createElement("button");
+    copyBtn.textContent = "Copy points";
+    copyBtn.addEventListener("click", e => {
+        copyButton(e, points);
+    });
+    pointsDetails.appendChild(copyBtn);
+    let list = document.createElement("ul");
+    points.forEach(function(point) {
+        let listItem = document.createElement("li");
+        listItem.textContent = `${point[0]} ${point[1]}`;
+        list.appendChild(listItem);
+    });
+    pointsDetails.appendChild(list);
+    historyItem.appendChild(pointsDetails);
+    
+    historyContainer.appendChild(historyItem);
+    historyContainer.appendChild(document.createElement("hr"));
+}
+
+loadHistory();
+function loadHistory() {
+    let currentData = getSave("distance-history");
+    if (currentData === undefined) {
+        return;
+    }
+
+    currentData.forEach((entry) => {
+        addMeasureToHistory(entry["start-time"], entry["distance-since-start"], entry["points"]);
+    });
+}
+
+// newLocationRadiusIgnoreOut.value = `0 ${chosenUnit}`;
+// newLocationRadiusIgnore.addEventListener("input", e => {
+//     newLocationRadiusIgnoreOut.value = `${e.target.value} ${chosenUnit}`;
+// });
+
 function resetValues() {
     distanceValues.startedAt = [];
     distanceValues.currentlyAt = [];
@@ -60,11 +136,6 @@ function resetValues() {
     previousPointsListElem.innerHTML = "";
     distanceValues.startTime = 0;
 }
-
-// newLocationRadiusIgnoreOut.value = `0 ${chosenUnit}`;
-// newLocationRadiusIgnore.addEventListener("input", e => {
-//     newLocationRadiusIgnoreOut.value = `${e.target.value} ${chosenUnit}`;
-// });
 
 function updateText () {
     startLocationSpan.textContent = `${distanceValues.startedAt[0]}, ${distanceValues.startedAt[1]}`;
@@ -116,13 +187,22 @@ startTrackingBtn.addEventListener("click", e => {
 });
 
 copyPreviousPointsBtn.addEventListener("click", e => {
-    if (navigator.clipboard) {
-        navigator.clipboard.writeText(JSON.stringify(distanceValues.previousPoints))
-        .then(() => {
-            copyPreviousPointsBtn.textContent = "Copied!";
-            setTimeout(function() {
-                copyPreviousPointsBtn.textContent = "Copy points";
-            }, 3000);
-        });
+    copyButton(e, distanceValues.previousPoints);
+});
+
+saveTrackingBtn.addEventListener("click", e => {
+    let currentSave = getSave("distance-history");
+    let newData = {
+        "start-time": distanceValues.startTime,
+        "distance-since-start": distanceValues.distanceSinceStart,
+        "points": distanceValues.previousPoints
     }
+
+    if (currentSave !== undefined) {
+        currentSave.push(newData);
+        saveData("distance-history", currentSave);
+    } else {
+        saveData("distance-history", [newData]);
+    }
+    addMeasureToHistory(distanceValues.startTime, distanceValues.distanceSinceStart, distanceValues.previousPoints);
 });
