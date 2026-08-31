85ac48aacaaa6f455354643ee52eff0d7696671b
aren1toross <arentorossian28@gmail.com>
2026-08-31 13:01:19 -0400

    distance script: added basic version of distance calc and point copying

---
+++ tools/distance/script.js
@@ -0,0 +1,127 @@
+import { getGeneralSetting } from "../../storage.js";
+
+let newLocationRadiusIgnore = document.querySelector("#new-location-ignore-radius");
+let newLocationRadiusIgnoreOut = document.querySelector("#new-location-ignore-radius-out");
+let startTrackingBtn = document.querySelector("#start-tacking-distance");
+let saveTrackingBtn = document.querySelector("#save-tracking-data");
+let copyPreviousPointsBtn = document.querySelector("#copy-previous-points");
+
+let startLocationSpan = document.querySelector("#start-location");
+let currentLocationSpan = document.querySelector("#current-location");
+let distanceFromStartSpan = document.querySelector("#distance-from-start");
+let previousPointsListElem = document.querySelector("#previous-points");
+console.log(previousPointsListElem);
+
+let distanceValues = {
+    isTracking: false,
+    watchValue: 0,
+    startedAt: [],
+    currentlyAt: [],
+    distanceSinceStart: 0,
+    startTime: 0,
+    previousPoints: []
+}
+let geolocationOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+};
+
+let chosenUnit = getGeneralSetting("distance-unit");
+
+function calculateDistance(c1, c2) {
+    let earthRadius = 6371;
+    let deltaLat = (c2[0] - c1[0]) * Math.PI / 180;
+    let deltaLng = (c2[1] - c1[1]) * Math.PI / 180;
+    let lat1 = c1[0] * Math.PI / 180;
+    let lat2 = c2[0] * Math.PI / 180;
+
+    let a = Math.pow(Math.sin(deltaLat / 2), 2) +
+            Math.pow(Math.sin(deltaLng / 2), 2) *
+            Math.cos(lat1) * Math.cos(lat2);
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    return earthRadius * c;
+}
+
+function getChosenUnitDistanceString(distance) {
+    if (chosenUnit === "metric") {
+        return `${Math.round(distance * 100) / 100} km`;
+    } else if (chosenUnit === "imperial") {
+        return `${Math.round(distance / 1.609 * 100) / 100} mi`;
+    }
+}
+
+function resetValues() {
+    distanceValues.startedAt = [];
+    distanceValues.currentlyAt = [];
+    distanceValues.distanceSinceStart = 0;
+    distanceValues.previousPoints = [];
+    distanceValues.startTime = 0;
+}
+
+// newLocationRadiusIgnoreOut.value = `0 ${chosenUnit}`;
+// newLocationRadiusIgnore.addEventListener("input", e => {
+//     newLocationRadiusIgnoreOut.value = `${e.target.value} ${chosenUnit}`;
+// });
+
+function updateText () {
+    startLocationSpan.textContent = `${distanceValues.startedAt[0]}, ${distanceValues.startedAt[1]}`;
+    currentLocationSpan.textContent = `${distanceValues.currentlyAt[0]}, ${distanceValues.currentlyAt[1]}`;
+    distanceFromStartSpan.textContent = getChosenUnitDistanceString(distanceValues.distanceSinceStart);
+    let newListItem = document.createElement("li");
+    let lastPoint = distanceValues.previousPoints.at(-1);
+    newListItem.textContent = `${lastPoint[0]}, ${lastPoint[1]}`;
+    previousPointsListElem.appendChild(newListItem);
+}
+
+function locationWatchSuccess (pos) {
+    startTrackingBtn.textContent = "Stop";
+    distanceValues.isTracking = true;
+    saveTrackingBtn.disabled = true;
+    const coords = pos.coords;
+
+    let currentPosition = [coords.latitude, coords.longitude]
+    if(distanceValues.startedAt.length === 0) {
+        distanceValues.startedAt = currentPosition;
+        distanceValues.startTime = Date.now();
+    }
+    distanceValues.currentlyAt = currentPosition;
+    if (distanceValues.previousPoints.length > 1) {
+        distanceValues.distanceSinceStart += calculateDistance(
+            distanceValues.previousPoints.at(-1),
+            currentPosition
+        );
+    }
+    distanceValues.previousPoints.push(currentPosition);
+    updateText();
+}
+
+function locationWatchError (err) {
+    console.error(`ERROR(${err.code}): ${err.message}`);
+}
+
+startTrackingBtn.addEventListener("click", e => {
+    if (distanceValues.isTracking) {
+        navigator.geolocation.clearWatch(distanceValues.watchValue);
+        
+        distanceValues.isTracking = false;
+        startTrackingBtn.textContent = "Start";
+        saveTrackingBtn.disabled = false;
+    } else {
+        resetValues();
+        distanceValues.watchValue = navigator.geolocation.watchPosition(locationWatchSuccess, locationWatchError, geolocationOptions);
+    }
+});
+
+copyPreviousPointsBtn.addEventListener("click", e => {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(JSON.stringify(distanceValues.previousPoints))
+        .then(() => {
+            copyPreviousPointsBtn.textContent = "Copied!";
+            setTimeout(function() {
+                copyPreviousPointsBtn.textContent = "Copy points";
+            }, 3000);
+        });
+    }
+});
